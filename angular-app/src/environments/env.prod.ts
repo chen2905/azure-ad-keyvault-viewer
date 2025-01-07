a55691d68c8d493289a9b5c77f5c80a7d03d67cb
ChenGao <maxlin2905@gmail.com>
2025-01-07 22:26:29 +1100

    before cd/ci

--- angular-app/src/environments/env.prod.ts
+++ angular-app/src/environments/env.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    production: false,
-    AZURE_AD_CLIENT_ID: 'faad2b43-b3cc-4462-9369-d7c6633f0673',  // Replace with your actual OpenWeatherMap API key,
+    production: true,
+    AZURE_AD_CLIENT_ID: 'a7dbe2c2-c88c-46c6-a6b2-5c2cdf8a504c',  // Replace with your actual OpenWeatherMap API key,
     AZURE_TENANT_ID: '84320fec-9721-43cd-abd3-1c8f5ba3a726',
-    AZURE_REDIRECT_URL:'XXXXXXXXXXX',
+    AZURE_REDIRECT_URL:'https://azure-ad-keyvault-viewer-ui-bbfagae3fxe2g5f5.canadacentral-01.azurewebsites.net/',
     AZURE_KEY_VAULT_NAME:'az204-project-key-vault',
-    KEYVAULT_VIEWER_API:'XXXXX_api/',
+    KEYVAULT_VIEWER_API:'https://azure-ad-keyvault-viewer-api-a5a7gscxf2dvcmdm.canadacentral-01.azurewebsites.net/api/',
   };
--- angular-app/src/environments/env.ts
+++ angular-app/src/environments/env.ts
@@ -4,5 +4,5 @@ export const environment = {
     AZURE_TENANT_ID: '84320fec-9721-43cd-abd3-1c8f5ba3a726',
     AZURE_REDIRECT_URL:'http://localhost:4200/',
     AZURE_KEY_VAULT_NAME:'az204-project-key-vault',
-    KEYVAULT_VIEWER_API:'https://localhost:7003/api/',
+    KEYVAULT_VIEWER_API:'http://localhost/api/',
   };
